6019e97e5719caca3503b1403691707f7875f05e
Luís Silva <31749403+LuisSousaSilva@users.noreply.github.com>
2020-01-23 16:31:26 +0000

    Future_Analyser_rebalaceamento_por_tempo_periodos_concluidos

--- Portfolio_function.py
+++ Portfolio_function.py
@@ -11,16 +11,44 @@ import quandl
 import plotly
 import time
 
-from pandas_datareader import data as pdr
-
-import yfinance as yf
-yf.pdr_override() # <== that's all it takes :-)
-
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from IPython.display import Markdown, display
 from matplotlib.ticker import FuncFormatter
 from pandas.core.base import PandasObject
 from datetime import datetime
+
+# Setting pandas dataframe display options
+pd.set_option("display.max_rows", 20)
+pd.set_option('display.width', 800)
+pd.set_option('max_colwidth', 800)
+
+pd.options.display.float_format = '{:,.2f}'.format
+
+# Set plotly offline
+init_notebook_mode(connected=True)
+
+# Set matplotlib style
+plt.style.use('seaborn')
+
+# Set cufflinks offline
+cf.go_offline()
+
+# Defining today's Date
+from datetime import date
+from pandas.tseries.offsets import BDay
+today = date.today()
+
+from pandas_datareader import data as pdr
+
+import yfinance as yf
+yf.pdr_override() # <== that's all it takes :-)
+
+def merge_time_series(df_1, df_2):
+    df = df_1.merge(df_2, how='left', left_index=True, right_index=True)
+    return df
+    
+# %%
+
 
 # Setting pandas dataframe display options
 pd.set_option("display.max_rows", 20)
@@ -224,6 +252,8 @@ End = "2020-01-15"
 for ticker in tickers:
     Quotes[ticker] = pdr.get_data_yahoo(ticker, start=Start, end=End)['Adj Close']
 
+# %%
+##########################
 Returns = Quotes.pct_change().dropna()
 
 quotes_norm = Quotes / Quotes.iloc[0]
@@ -231,7 +261,7 @@ weights_all = pd.DataFrame()
 n = -1
 loop_end = 'No'
 
-rebalance_dates = pd.bdate_range(start=Start, end=End, freq='BQ')
+rebalance_dates = pd.bdate_range(start=Start, end=End, freq='BA')
 
 for i in np.arange(len(rebalance_dates) + 1):
     n = n + 1
@@ -251,12 +281,10 @@ for i in np.arange(len(rebalance_dates) + 1):
     
     try:
         if rebalance_dates[n] in weights.index:
-            print('OK')
             break_point = weights[(weights.index == rebalance_dates[n])].index[0]
         else:
-            # break_point = weights[(weights.index == rebalance_dates[n] - BDay(1))].index[0]
-            print('offset')
-        print(break_point)
+            break_point = weights[(weights.index == rebalance_dates[n] - BDay(1))].index[0]
+            print(break_point)
         weights = weights[:break_point]
         quotes_norm = quotes_norm[break_point:]
         quotes_norm = quotes_norm / quotes_norm.iloc[0]
@@ -276,39 +304,88 @@ Portfolio = Portfolio.append(first_day)
 Portfolio_time_rebanced = Portfolio.sort_index()
 
 # %%
-Portfolio_time_rebanced.iplot(color='royalblue', title='B&H')
+Portfolio_time_rebanced.iplot(color='royalblue', title='Relanceamentos temporais')
 
 # %%
-weights_all.iplot(title='Pesos de Buy & Hold')
+weights_all.iplot(title='Pesos de rebalanceamentos temporais')
 
+# %%
+Portfolios = merge_time_series(Portfolio_BH, Portfolio_time_rebanced)
+Portfolios.columns = ['B&H', 'TR']
+Portfolios.iplot()
 
+# %%
+def compute_portfolio_2(Quotes, asset_weights, rebalance_periods='years'):
 
+    if rebalance_periods == 'years':
+        rebalance_dates = pd.bdate_range(start=Start, end=End, freq='BA')
 
+    if rebalance_periods == 'quarters':
+        rebalance_dates = pd.bdate_range(start=Start, end=End, freq='BQ')
+    
+    if rebalance_periods == 'months':
+        rebalance_dates = pd.bdate_range(start=Start, end=End, freq='BM')
 
+    Returns = Quotes.pct_change().dropna()
 
+    quotes_norm = Quotes / Quotes.iloc[0]
+    weights_all = pd.DataFrame()
+    n = -1
+    loop_end = 'No'    
 
+    for i in np.arange(len(rebalance_dates) + 1):
+        n = n + 1
+        
+        weights = pd.DataFrame()
 
+        Portfolio_norm = quotes_norm.sum(axis=1)
 
+        for ticker in Quotes.columns:
+            weights[ticker] = quotes_norm[ticker].shift(-1) / Portfolio_norm.shift(-1)
 
+        weights = weights.shift(2)
+        
+        weights.iloc[1] = asset_weights
+        
+        weights = weights.dropna()
+        
+        try:
+            if rebalance_dates[n] in weights.index:
+                break_point = weights[(weights.index == rebalance_dates[n])].index[0]
+            else:
+                break_point = weights[(weights.index == rebalance_dates[n] - BDay(1))].index[0]
+            weights = weights[:break_point]
+            quotes_norm = quotes_norm[break_point:]
+            quotes_norm = quotes_norm / quotes_norm.iloc[0]
+        except:
+            loop_end = 'Yes'
+            print('Fez ' + str(n) + ' rebalanceamentos')
 
+        weights_all = weights_all.append(weights)
 
-# # %%
-# Portfolio_year = compute_portfolio(Quotes, weights=[0.5, 0.5])
-# Portfolio_year.iloc[-1]
+    contributions = weights_all * Returns
+    Portfolio_ret = contributions.sum(axis=1)
+    Portfolio = pd.DataFrame(compute_time_series(Portfolio_ret))
 
-# # %%
-# Weights.iplot()
+    first_day = pd.DataFrame(np.array([[100]]), index=[pd.to_datetime(Start)])
+    Portfolio = Portfolio.append(first_day)
 
-# # %%
-# weights_all.iplot()
+    Portfolio = Portfolio.sort_index()
+    return Portfolio
 
-# # %%
-# Portfolio.iplot()
+# %%
+compute_portfolio(Quotes, [0.5, 0.5])
 
-# # %%
-# Portfolio_year.iplot()
+# %%
+years = compute_portfolio_2(Quotes, [0.5, 0.5], 'years')
+quarters = compute_portfolio_2(Quotes, [0.5, 0.5], 'quarters')
+months = compute_portfolio_2(Quotes, [0.5, 0.5], 'months')
 
-# # %%
+# %%
+time_series = merge_time_series(years, quarters)
+time_series = merge_time_series(time_series, months)
+time_series.columns = ['years', 'quarters', 'months']
+time_series.iplot()
 
 
 # %%
