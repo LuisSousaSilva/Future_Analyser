c58e4a79eff0ecef1a69e06ae5b396b952050072
Luís Silva <31749403+LuisSousaSilva@users.noreply.github.com>
2020-01-29 19:48:44 +0000

    Future_Analyser

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='ptolemaios',
-    version='0.0.1',
-    description='My private package from private github repo',
-    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
-    author='Raphael Schubert',
-    author_email='raphael.schubert@digitalbankscorp.com',
-    license='unlicense',
-    packages=['ptolemaios'],
-    zip_safe=False
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Future_Analyser',
+    url='https://github.com/jladan/package_demo',
+    author='John Ladan',
+    author_email='jladan@uwaterloo.ca',
+    # Needed to actually package something
+    packages=['measure'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
 )
